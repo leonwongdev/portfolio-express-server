642cdce391e5f4d353ec52f35433a0339c4b1a98
Lap Wang Wong <leonwonglww@gmail.com>
2024-04-15 17:31:55 -0400

    Added skills endpoint

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("../db");
 const Project = require("../models/Project");
+const Skill = require("../models/Skill");
 const app = express();
 const port = process.env.PORT || 3000;
 // Load environment variables from .env file
@@ -22,6 +23,16 @@ app.get("/api/projects", async (req, res) => {
   try {
     const projects = await Project.find();
     res.json(projects);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// GET endpoint to retrieve all skills
+app.get("/api/skills", async (req, res) => {
+  try {
+    const skills = await Skill.find();
+    res.json(skills);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
